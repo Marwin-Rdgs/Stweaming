5a674e7be01988e3d3bb7e1bc65cce5aa08c111b
Marwin <marwin-rdgs.25@laposte.net>
2022-01-15 02:02:35 +0100

    Début footer V

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <footer class="bg-primary">
 
-<h1 class="title">tgest</h1>
+<h1 class="title">Marwin Rodrigues</h1>
+<h1 class="title">à propos</h1>
+<h1 class="title">Me Contacer</h1>
+<h1 class="title">Mention Légale</h1>
 
 </footer>
