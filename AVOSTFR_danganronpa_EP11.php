387ddcde079b928b8e9461b987b4b9b2a6d8a441
Marwin <marwin-rdgs.25@laposte.net>
2022-02-01 13:36:14 +0100

    Ajout Saison Danganronpa + liste episode

--- AVOSTFR_danganronpa_EP11.php
+++ AVOSTFR_danganronpa_EP11.php
@@ -11,7 +11,7 @@
     <!-- Appel à la feuille de style -->
     <link rel="stylesheet" href="./src/css/style.css">
 
-    <title>Stweaming ~ Danganronpa The Animation EP 1</title>
+    <title>Stweaming ~ Danganronpa The Animation EP 11</title>
 </head>
 <body class="container bg-test w100">
 
@@ -21,13 +21,14 @@
 
 
 <div class="top_page">
-<h1 class="page__title">Danganronpa The Animation <br> Épisode 1</h1>
+<h1 class="page__title">Danganronpa The Animation <br> Épisode 11</h1>
 </div>
 <a href="animes_vostfr.php"><span class="badge rounded-pill bg-danger badge-categ">Animé VOSTFR</span></a>
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/2XXTmfqhMXQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ZkdaizseqLw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     
-    <a href="./AVOSTFR_danganronpa_EP2.php"><button type="button" class="btn btn-outline-primary btn_episode">Épisode Suivant</button></a>
+   <a href="./AVOSTFR_danganronpa_EP10.php"> <button type="button" class="btn btn-outline-primary btn_episode" style="float:left">Épisode Précédent</button></a>
+   <a href="./AVOSTFR_danganronpa_EP12.php"> <button type="button" class="btn btn-outline-primary btn_episode">Épisode Suivant</button></a>
 
     <p class="syno">Danganronpa: The Animation, connue au Japon sous le nom de Danganronpa: Kibō no gakuen to zetsubō no kōkōsei The Animation (ダンガンロンパ 希望の学園と絶望の高校生 The Animation?), est une série télévisée d'animation japonaise de 13 épisodes basée sur la série de jeux vidéo de Spike (devenu Spike Chunsoft), Danganronpa. La série produite par le studio d'animation Lerche reprend l'histoire du premier jeu Danganronpa: Trigger Happy Havoc, sorti au Japon en 2010 sur PlayStation Portable1,2. 
         <br> <br>
@@ -41,7 +42,7 @@
     </tr>
   </thead>
   <tbody>
-    <tr class="table-active episode_here">
+    <tr class="table-active">
     <th scope="row"> <a href="./AVOSTFR_danganronpa_EP1.php"> Danganronpa The Animation Épisode 1 </a> </th>
     </tr>
     <tr class="table-secondary">
@@ -71,7 +72,7 @@
     <tr class="table-secondary">
       <th scope="row"><a href="./AVOSTFR_danganronpa_EP10.php"> Danganronpa The Animation Épisode 10 </a></th>
     </tr>
-    <tr class="table-active">
+    <tr class="table-active episode_here">
       <th scope="row"><a href="./AVOSTFR_danganronpa_E11.php"> Danganronpa The Animation Épisode 11 </a></th>
     </tr>
     <tr class="table-secondary">
--- src/php/BDD/bdd.php
+++ src/php/BDD/bdd.php
@@ -1,5 +1,6 @@
 <?php     
 $bdd = new PDO("mysql:host=localhost; dbname=stweaming; charset=utf8", "root", '');
+// $bdd = new PDO("mysql:host=2s24l.myd.infomaniak.com; dbname=2s24l_stweaming; charset=utf8", "2s24l_stweaming", 'Root123456');
 
 //============================ Partie Dessins Animes =========================================
 // LETTRE D
